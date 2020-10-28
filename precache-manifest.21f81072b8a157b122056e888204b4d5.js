8b92b6154d00c2342ee80b3834f39dca645e119c
elena-salam <elena.salamakha.ua@gmail.com>
2020-10-28 11:31:25 +0200

    Updates

---
+++ precache-manifest.21f81072b8a157b122056e888204b4d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc5846c8bf33ecae13f025b53045f0a5",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "50bb90ce208f924adfc5",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b8dcc721.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.b8dcc721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d0fbdb039acbc9b3dd",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.e5d738d0.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
